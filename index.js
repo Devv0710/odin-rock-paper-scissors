573e679b5e204d4c177ff953ce93fc9f106ed435
Devv0710 <vazquezdavid247@gmail.com>
2024-04-18 11:20:31 -0400

    Added logic to play a round

--- index.js
+++ index.js
@@ -11,4 +11,46 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  const pSelection = playerSelection.toLowerCase();
+  const cSelection = computerSelection.toLowerCase();
+
+  switch (pSelection) {
+    case "rock":
+      switch (cSelection) {
+        case "paper":
+          return `You lose,${cSelection} baets ${pSelection}`;
+        case "scissors":
+          return `You win, ${pSelection} baets ${cSelection}`;
+        default:
+          return "Tie";
+      }
+    case "paper":
+      switch (cSelection) {
+        case "scissors":
+          return `You lose,${cSelection} baets ${pSelection}`;
+        case "rock":
+          return `You win, ${pSelection} baets ${cSelection}`;
+        default:
+          return "Tie";
+      }
+    case "scissors":
+      switch (cSelection) {
+        case "rock":
+          return `You lose,${cSelection} baets ${pSelection}`;
+        case "paper":
+          return `You win, ${pSelection} baets ${cSelection}`;
+        default:
+          return "Tie";
+      }
+    default:
+      return "Enter valid option";
+  }
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
